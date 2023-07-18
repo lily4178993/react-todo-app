2775ac9d2b8c96a1e8ac9d266d412ad0a461a084
lily4178993 <nellytelli@gmail.com>
2023-07-18 05:09:26 +0000

    Add Strict Modeto identify app bugs

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+import Header from 'components/Header';
+import TodosLogic from 'components/TodosLogic';
+
+const TodoApp = () => (
+  <>
+    <Header />
+    <TodosLogic />
+  </>
+);
+export default TodoApp;
