46e36c311db606cc8cc447cf96bdf21e8a3c14f9
lily4178993 <nellytelli@gmail.com>
2023-07-18 10:15:40 +0000

    Add functionalities to form imput field

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,2 +1,37 @@
-const InputTodo = () => <div>input field here...</div>;
+import { useState } from 'react';
+
+// eslint-disable-next-line react/prop-types
+const InputTodo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('Please add item.');
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <span>{message}</span>
+    </>
+  );
+};
 export default InputTodo;
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 import InputTodo from 'components/InputTodo';
 import TodosList from 'components/TodosList';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -31,15 +33,25 @@ const TodosLogic = () => {
       return todo;
     }));
   };
+
   const delTodo = (id) => {
     setTodos([
       ...todos.filter((todo) => todo.id !== id),
     ]);
   };
 
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList
         todosProps={todos}
         handleChange={handleChange}
